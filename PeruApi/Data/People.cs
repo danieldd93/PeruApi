3ab3998b0dafaed2f4f2df42c4807ef2d937d4a5
Daniel Delgado <danieldd.93@gmail.com>
2024-04-27 17:47:51 -0500

    Agregar archivos de proyecto.

---
+++ PeruApi/Data/People.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PeruApi.Data;
+
+public partial class People
+{
+    public int Id { get; set; }
+
+    public string? Fullname { get; set; }
+
+    public string? Email { get; set; }
+
+    public int? Dni { get; set; }
+
+    public bool? IsActive { get; set; }
+}
